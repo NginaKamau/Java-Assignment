bbd4121abe36cb3d219ba5320b4fbf6f979b1b35
NginaKamau <loisek.ngina@strathmore.edu>
2025-02-03 13:36:20 +0300

    Created a new object to test whether the code works

--- CarTest.java
+++ CarTest.java
@@ -1,10 +1,15 @@
 public class CarTest {
     public static void main(String[]args){
         ElectricCar neta=new ElectricCar("Neta Model");
+        ElectricCar tesla=new ElectricCar("Tesla Model");
 
         neta.showBrand();
         neta.startEngine();
         neta.fuelType();
+
+        tesla.showBrand();
+        tesla.startEngine();
+        tesla.fuelType();
 
 
     }
